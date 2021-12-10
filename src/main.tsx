8ca4c0678f093ef890790ac18b105234e24f4235
komesu_tatsuki <komesu_tatsuki@trek.co.jp>
2021-12-10 10:57:20 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'src/components/app';
 import Providers from 'src/components/providers';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW();
 
 ReactDOM.render(
   <React.StrictMode>
