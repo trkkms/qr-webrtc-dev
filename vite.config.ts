efd058e3950247b57a0d317f703437adb3387af6
komesu_tatsuki <komesu_tatsuki@trek.co.jp>
2022-03-09 11:02:51 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/qr-webrtc-dev/',
+  mode: 'development',
   plugins: [
     react(),
     VitePWA({
